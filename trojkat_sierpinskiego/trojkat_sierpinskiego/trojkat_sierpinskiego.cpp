7e721dfe18cd30bc664aa02c0893cba5ba0bb26d
Kamil <78850014+Stunnerek@users.noreply.github.com>
2023-12-14 22:19:29 +0100

    Add files via upload

---
+++ trojkat_sierpinskiego/trojkat_sierpinskiego/trojkat_sierpinskiego.cpp
@@ -0,0 +1,54 @@
+﻿// trojkat_sierpinskiego.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        
+        window.clear(sf::Color::Black);
+        sf::Vertex line[] =
+        {
+            sf::Vertex(sf::Vector2f(0, 100)),
+            sf::Vertex(sf::Vector2f(150, 100))
+        };
+        sf::Vertex line2[] =
+        {
+            sf::Vertex(sf::Vector2f(0, 100)),
+            sf::Vertex(sf::Vector2f(100, 150))
+        };
+       
+
+        window.draw(line, 2, sf::Lines);
+        window.draw(line2, 2, sf::Lines);
+
+   
+        window.display();
+    }
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
